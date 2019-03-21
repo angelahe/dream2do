34b376127f791153a3315d5fdadc82112cb79971
Matt Fisher <matt.fisherrr@gmail.com>
2019-03-21 10:47:43 -0600

    Added react-sound package, added sound to audio button

--- src/components/Level1Card.js
+++ src/components/Level1Card.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Sound from 'react-sound'
 
 const Level1Card = (props) => {
+    const playAudio = (x) => {
+        console.log('sound')
+        return(
+            <Sound
+                url={this.props.deck.cardHolder[x].audio}
+            />
+        );
+    }
     return(
         <div className = "playCard">
             <div className = "cardImg"> Image</div>
             <div className = "cardText">Text</div>
-            <span className = "audioBtn">audio</span>
+            <span className = "audioBtn" onClick={() => playAudio()}>audio</span>
             <span className = "navBtn">nav</span>
             <br/>
         </div>
