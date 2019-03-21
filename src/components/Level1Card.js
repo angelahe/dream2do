d85f6bc52d8ef5e29bb3da2deec2d48655fff3ed
Marcela Wang <marcelawang7@gmail.com>
2019-03-21 10:39:49 -0600

    nav bar changes

--- src/components/Level1Card.js
+++ src/components/Level1Card.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React from "react";
 
-const Level1Card = (props) => {
-    return(
-        <div className = "playCard">
-            <div className = "cardImg"> Image</div>
-            <div className = "cardText">Text</div>
-            <span className = "audioBtn">audio</span>
-            <span className = "navBtn">nav</span>
-            <br/>
-        </div>
-    );
+const cardDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let cardNumber = 0;
+
+function handlePrevClick() {
+  if (cardNumber === 0) {
+    cardNumber = 10;
+    
+  } else {
+    cardNumber--
+  } 
+  console.log(cardNumber)
 }
 
-export default Level1Card;
+function handleNextClick() {
+    if (cardNumber === 10) {
+        cardNumber = 0;
+      } else {
+        cardNumber++
+      } 
+      console.log(cardNumber)
+}
+
+const Level1Card = props => {
+  return (
+    <div className="playCard">
+      <div className="cardImg"> Image</div>
+      <div className="cardText">{cardDeck[cardNumber]}</div>
+      <span className="audioBtn">audio</span>
+      <br />
+      <br />
+      <span onClick={handlePrevClick} className="navBtn">
+        prev
+      </span>
+      <br />
+      <span onClick={handleNextClick} className="navBtn">
+        next
+      </span>
+
+      <br />
+    </div>
+  );
+};
+
+export default Level1Card
