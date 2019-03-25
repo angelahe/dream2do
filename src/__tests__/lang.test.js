a1e18bcc70004a7ce82a1ee196447368cbf38d35
misterhtmlcss <misterhtmlcss@gmail.com>
2019-03-25 16:50:29 -0600

    added new tests and completed half of lang.js methods

--- src/__tests__/lang.test.js
+++ src/__tests__/lang.test.js
@@ -1,16 +1,52 @@
 import lang from '../components/lang'
 
 test('should detect object creation', () => {
+  //lang { lang: 'english', langArr: [ 'english' ] }
   const newLang = new lang('english')
-  const testLangForObject = typeof newLang
-  expect(testLangForObject).toBe('object')
+  const defaultLang = newLang.lang //lang: 'english'
+  const english = newLang.langArr[0]
+  const defaultLength = newLang.langArr.length
+
+  expect(defaultLang).toBe('english')
+  expect(english).toBe('english')
+  expect(defaultLength).toBe(1)
 })
 
-test('checkLang should 2 languages', () => {
+test('addLang should add any to langArr', () => {
+  const newLang = new lang('en')
+  const testArr = ['en', 'wookielove']
+  const result = newLang.addLang('wookielove')
+  // take result and compare result array to test array
+  expect(result).toContain('wookielove')
+  expect(result).toContain('wookielove')
+  expect(result.length).toBe(2)
+  expect(result.length).toBeLessThan(3)
+})
+
+xtest('editLang should remove the last language added', () => {
+  const newLang = new lang('en')
+  const testArr = ['en', 'es']
+  const result = newLang.addLang('es')
+  // take result and compare result array to test array
+  expect(result).toBe(testArr)
+})
+
+xtest('deleteLangs should delete english from langArr', () => {
+  const newLang = new lang('en')
+  const updatedArr = ['es']
+  const result = newLang.deleteLangs('en')
+  expect(result).toBe(updatedArr)
+})
+
+xtest('checkLang should find 2 languages', () => {
   // instantiate the new lang Class
-  const newLang = new lang('english')
+  const newLang = new lang('en')
+  const secondLanguage = 'es'
+  const newArray = ['en', 'es']
+
+  const addedArray = newLang.addLang(secondLanguage)
 
   // take new Object that was intantiated and use method/function checkLang to test language array length.
-  const langs = newLang.checkLang(arr)
-  expect(langs.length).toBe(2)
+  const langs = newLang.checkLang(newArray)
+  expect(langs.length).toBe(addedArray.length)
 })
