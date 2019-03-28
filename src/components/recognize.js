ebc68e414b47db9f4e9dac2a2f07c9e972494071
Matt Fisher <matt.fisherrr@gmail.com>
2019-03-28 15:40:08 -0600

    Added logging to recognize class

--- src/components/recognize.js
+++ src/components/recognize.js
@@ -1,10 +1,20 @@
 import Deck from './deck'
+import log from './log'
 
 class recognize {
   constructor() {
     this.cardNumber = 0;
     this.cardSide = true; //Language one is true, language two is false
     this.deck = new Deck();
+    this.log = new log();
+  }
+
+  logTestResult(correct){
+    this.log.write(
+      'Level 2',
+      this.deck.cardHolder[this.cardNumber].textLanguageOne,
+      this.deck.cardHolder[this.cardNumber].textLanguageTwo,
+      correct)
   }
 
   cardCorrect = () => {
@@ -19,23 +29,13 @@ class recognize {
     this.cardSide = !this.cardSide;
 
     console.log(`${this.deck.cardHolder[this.cardNumber].textLanguageOne},${this.deck.cardHolder[this.cardNumber].textLanguageTwo}, Level 2, Correct`);
-    localStorage.setItem(`log${this.cardNumber}`,JSON.stringify({
-      L1:this.deck.cardHolder[this.cardNumber].textLanguageOne,
-      L2:this.deck.cardHolder[this.cardNumber].textLanguageTwo,
-      date:`28/3/2019`,
-      corrent:true,
-    }))
+    this.logTestResult(true);
   }
 
   cardWrong = () => {
     
     console.log(`${this.deck.cardHolder[this.cardNumber].textLanguageOne},${this.deck.cardHolder[this.cardNumber].textLanguageTwo}, Level 2, Incorrect`);
-    return({
-      L1:this.deck.cardHolder[this.cardNumber].textLanguageOne,
-      L2:this.deck.cardHolder[this.cardNumber].textLanguageTwo,
-      date:`28/3/2019`,
-      corrent:false,
-    })
+    this.logTestResult(false);
   }
 
 }
