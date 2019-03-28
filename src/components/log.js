39f6cd5f70d5826f4aec7744854efcda406eb85b
angela <angela@aviak.ca>
2019-03-28 15:21:06 -0600

    add .idea to gitignore, log tests

--- src/components/log.js
+++ src/components/log.js
@@ -1,22 +1,30 @@
+/**
+ * Reads and writes to a localstorage log
+ * @constructor
+ *   @check - if log is null, create a new log
+ *   @param {object} today - get today's date in YYYY-MM-DD format
+ */
 class log{
   constructor(){
-    if(localStorage.getItem('log') === null){localStorage.setItem('log','')}
+    if(localStorage.getItem('log') === null){localStorage.setItem('log','[]')}
     this.today = new Date();
   }
 
+
   write(level,languageOne,languageTwo,correct){
+    let testResult = JSON.parse(localStorage.getItem('log'));
+    const testLogItem = {
+      level: level,
+      L1: languageOne,
+      L2: languageTwo,
+      date: `${this.today.getFullYear()}-${(this.today.getMonth()+1)}-${this.today.getDate()}`,
+      correct: correct,
+    }
+
+    testResult.push(testLogItem);
+
     localStorage.setItem(
-      `log`,JSON.stringify(
-        JSON.parse(
-          localStorage.getItem('log')
-        ).push({
-          level:level,
-          L1:languageOne,
-          L2:languageTwo,
-          date:`${this.today.getFullYear()}-${(this.today.getMonth()+1)}-${this.today.getDate()}`,
-          correct:correct,
-        })
-      )
+      `log`,JSON.stringify(testResult)
     );
   }
 
