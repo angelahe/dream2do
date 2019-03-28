80b878b0ee0af2bff749ecf5a521a0bcc03a1998
Marcela Wang <marcelawang7@gmail.com>
2019-03-28 15:44:48 -0600

    created user functions

---
+++ src/__tests__/user.test.js
@@ -0,0 +1,30 @@
+import User from "../components/user"
+
+test('should create a new user', () => {
+  const person = new User('Max','Mad');
+  //console.log('person',person)
+
+  // console.log('newUser',newUser)
+  expect(person.user.lastName).toBe('Mad')
+  expect(person.user.firstName).toBe('Max')
+})
+
+test('should be able to change first and lastname', () => {
+  const originalUser = new User('Max','Mad');
+  const nameType = 'firstName'
+  const changedName = 'Roger'
+  const newUser = originalUser.updateUser(changedName, nameType)
+  expect(originalUser.user.firstName).toBe(newUser.firstName)
+  expect(originalUser.user.firstName).not.toBe([])
+})
+
+test('should delete firstName and lastName', () => {
+  const person = new User('Max','Mad');
+  //console.log(person.user)
+  expect (person.user.firstName).toBe('Max')
+  expect (person.user.lastName).toBe('Mad')
+  expect (person.user).toHaveProperty('lastName', 'Mad')
+  person.deleteUser()
+  expect (person.user).not.toHaveProperty('firstName')
+  expect (person.user).not.toHaveProperty('lastName', 'Mad')
+})
