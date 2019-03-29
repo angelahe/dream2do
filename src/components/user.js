8ecb1f209684d467119a322748cbfda4800c6ffc
misterhtmlcss <misterhtmlcss@gmail.com>
2019-03-29 16:27:33 -0600

    added email and refactored if statement to switch statement

--- src/components/user.js
+++ src/components/user.js
@@ -5,31 +5,54 @@
  */
 
 export default class User {
-  constructor(firstName, lastName) {
+  constructor(firstName = '', lastName = '', email = '') {
     this.user = {
       firstName,
-      lastName
+      lastName,
+      email,
     }
   }
 
 
-  updateUser(changedName, nameType) {
+  updateUser(changedData, nameType) {
     // console.log(this.user)
-    // console.log(changedName, nameType)
-    if (nameType === 'firstName') {
-      // change first name user object
-      this.user.firstName = changedName
-      return this.user
-
-    } else if (nameType === 'lastName') {
-      // change last name user object
-      this.user.lastName = changedName
-      return this.user
-
-    } else {
-      return this.user
+    // console.log(changedData, nameType)
+    switch (nameType) {
+      case 'firstName': {
+        this.user.firstName = changedData;
+        return this.user
+      }
+      case 'lastName': {
+        this.user.lastName = changedData;
+        return this.user
+      }
+      case 'email': {
+        this.user.email = changedData;
+        return this.user
+      }
+      default: console.log('update user failed to update')
+        break;
     }
   }
+
+  // if (nameType === 'firstName') {
+  //   // change first name user object
+  //   this.user.firstName = changedData
+  //   return this.user
+
+  // } else if (nameType === 'lastName') {
+  //   // change last name user object
+  //   this.user.lastName = changedData
+  //   return this.user
+
+  // } else if (nameType === 'email') {
+  //   this.user.email = changedData
+  //   return this.user
+  // }
+  // else {
+  //   return this.user
+  // }
+
 
   deleteUser() {
     return this.user = {}
