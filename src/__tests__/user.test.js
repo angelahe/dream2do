b8a4513266af4ca01822e13c0ae3475b79fe981e
misterhtmlcss <misterhtmlcss@gmail.com>
2019-03-29 16:28:36 -0600

    added test for email key from user object

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -2,9 +2,6 @@ import User from "../components/user"
 
 test('should create a new user', () => {
   const person = new User('Max', 'Mad');
-  //console.log('person',person)
-
-  // console.log('newUser',newUser)
   expect(person.user.lastName).toBe('Mad')
   expect(person.user.firstName).toBe('Max')
 })
@@ -18,9 +15,18 @@ test('should be able to change first and lastname', () => {
   expect(originalUser.user.firstName).not.toBe([])
 })
 
+test('should be able to change email', () => {
+  const originalUser = new User('Max', 'Mad', 'madmax@evolveu.ca')
+  const nameType = 'email'
+  const changedEmail = 'maddermax@evolveu.ca'
+  const newUser = originalUser.updateUser(changedEmail, nameType)
+  expect(originalUser.user.email).toBe(newUser.email)
+  expect(originalUser.user.email).not.toBe([])
+})
+
+
 test('should delete firstName and lastName', () => {
   const person = new User('Max', 'Mad');
-  //console.log(person.user)
   expect(person.user.firstName).toBe('Max')
   expect(person.user.lastName).toBe('Mad')
   expect(person.user).toHaveProperty('lastName', 'Mad')
