80711df18feddb1103a9f40c43fe992d8e140068
angela <angela@aviak.ca>
2019-04-01 15:54:47 -0600

    colors audio files

--- src/__tests__/Level2Card.test.js
+++ src/__tests__/Level2Card.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Level2Card from '../components/Level2Card'
 
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Level2Card />, div);
